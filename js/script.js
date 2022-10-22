4297b690e46f9588bf530e4704dc1fb91ef44a7f
AngeloCalabro <angelo.1991@hotmail.com>
2022-10-22 11:15:52 +0200

    problema con bottoni

--- js/script.js
+++ js/script.js
@@ -27,3 +27,80 @@ const images = [
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
     },
 ];
+
+const contaier = document.querySelector('.my-container');
+createCarousel(images);
+
+const mainImage = document.querySelectorAll('.view-image');
+const carouselImage = document.querySelectorAll('.my-carousel-image')
+shiftCarousel(mainImage, 'd-none');
+shiftCarousel(carouselImage, 'my-opacity');
+
+//funzione che crea le immagini piccole del carosello
+function createMainImage(object) {
+    const mainImage = document.createElement('div');
+    mainImage.className = 'col-12 view-image d-none';
+    mainImage.innerHTML =
+        `
+    <img src="${object.url}" alt="${object.title}" class="big-dimension">
+    <div class="title-image">
+        <h1>${object.title}</h1>
+        <span class="description-image">${object.description}</span>
+    </div>
+    `
+    return mainImage;
+}
+
+//funzione che crea l'immagine grande
+function createCarouselImage(object) {
+    const carouselImage = document.createElement('div');
+    carouselImage.className = 'col my-carousel-image my-opacity';
+    carouselImage.innerHTML = `<img src="${object.url}" alt="${object.title}" class="small-dimension">`
+    return carouselImage;
+}
+
+//funzione che crea il caroselo
+function createCarousel(objectArray) {
+    const myFlex = document.createElement('div');
+    myFlex.className = 'my-flex';
+    contaier.appendChild(myFlex);
+
+    for (let object of objectArray) {
+        const mainImage = createMainImage(object);
+        myFlex.appendChild(mainImage);
+    }
+    for (let object of objectArray) {
+        const carouselImage = createCarouselImage(object);
+        myFlex.appendChild(carouselImage);
+    }
+
+    myFlex.innerHTML +=
+        `
+    <button id="previous" class="btn btn-dark">
+        <i class="fa-solid fa-angle-left"></i>
+    </button>
+    <button id="next" class="btn btn-dark">
+        <i class="fa-solid fa-angle-right"></i>
+    </button>
+    `
+}
+
+//funzione che fa scorrere le immagini
+function shiftCarousel(objectArray, classe) {
+    objectArray[0].classList.toggle(classe);
+    setInterval(showImage, 3000, objectArray);
+    let i = 0;
+    let j = 1;
+    function showImage(objectArray) {
+        const x = mod5(i);
+        const y = mod5(j);
+        objectArray[x].classList.toggle(classe);
+        objectArray[y].classList.toggle(classe);
+        i++;
+        j++;
+    }
+}
+
+function mod5(numb) {
+    return numb % 5
+};
